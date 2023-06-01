5b456a6b0b69a87f5de428619e36007938f87e21
ved_f19 <81800671+vedant-fire@users.noreply.github.com>
2023-06-01 09:24:40 +0530

    Update Crop_pred.py

--- Crop_pred.py
+++ Crop_pred.py
@@ -2,7 +2,7 @@
 #importing the required libraries
 import sys
 import pandas as pd
-import numpy asnp
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 #Reading the csv file
